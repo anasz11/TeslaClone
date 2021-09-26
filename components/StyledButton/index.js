ae16fbc44998384a251938e1929f9b86ef7a85a4
azoubi <anasz11zoabi@gmail.com>
2021-09-26 14:47:39 +0200

    Button component done

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -2,25 +2,22 @@ import React from 'react';
 import {View, Text, Pressable} from 'react-native';
 import styles from './styles'
 
-const StyledButton = (props) => {
 
-    const type = props.type;
-    const content = props.content;
-    const onPress = props.onPress;
+const StyledButton = (props) => {
     
+    const { type, content, onPress } = props;
+
     const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
     const textColor = type === 'primary' ? '#FFFFFF' : '#171A20';
 
     return (
         <View style={styles.container}>        
-           <Pressable 
+            <Pressable 
                 style={[styles.button, {backgroundColor: backgroundColor}]}
-                onPress={() => {
-                    console.warn('Hey There!');
-                }}
+                onPress={() => onPress()}
             >
                 <Text style={[styles.text, {color: textColor}]}>{content}</Text>
-           </Pressable>
+            </Pressable>
         </View>
     );
 };
